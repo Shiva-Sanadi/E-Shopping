9687f4ddec07c1188b8b8118e6cb5f05883fbefb
ShivashanShivashankar Sanadi <shivashankarsanadi@gmail.com>
2025-12-23 16:01:31 +0530

    Add backend and frontend changes

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -1,7 +1,9 @@
+//E-Shopping/my-project/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/E-Shopping/',  // This fixes the asset paths for GitHub Pages
   plugins: [react()],
 })
